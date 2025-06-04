3a6dfc755b0363b25c91924787369e4de61287f3
EfxSkyFall <19wrobbens@salesian.surrey.sch.uk>
2025-06-04 10:46:16 +0100

    Add files via upload

--- python with sql.py
+++ python with sql.py
@@ -6,9 +6,10 @@ cursor = db.cursor()
 # create new table
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS student (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL,
     course TEXT NOT NULL,
+    choice TEXT NOT NULL,
     grade1 TEXT NOT NULL,
     grade2 TEXT NOT NULL,
     grade3 TEXT NOT NULL
@@ -17,34 +18,49 @@ CREATE TABLE IF NOT EXISTS student (
 
 db.commit()
 
-# inserting record
-name = input('Enter Name: ')
-course = input('Enter the university course you are applying for: ')
-grade1 = input("Enter grade of subject 1: ")
-grade2 = input("Enter grade of subject 2: ")
-grade3 = input("Enter grade of subject 3: ")
+# Inserting record
+def insert():
+    
+    name = input('Enter Name: ')
+    course = input('Enter the university course you are applying for: ')
+    choice = input('Enter your first choice University: ')
+    grade1 = input("Enter grade of subject 1: ")
+    grade2 = input("Enter grade of subject 2: ")
+    grade3 = input("Enter grade of subject 3: ")
 
-cursor.execute('''
-INSERT INTO student (name,course,grade1,grade2,grade3)
-VALUES
-    (?,?,?,?,?)
-''',(name,course,grade1,grade2,grade3))
+    cursor.execute('''
+    INSERT INTO student (name,course,choice,grade1,grade2,grade3)
+    VALUES
+        (?,?,?,?,?)
+    ''',(name,course,choice,grade1,grade2,grade3))
 
-db.commit()
+    db.commit()
 
-#VIEW database
-cursor.execute('SELECT * FROM student')
-data = cur.fetchall()
+# View database
+def view_data():
+    cursor.execute('SELECT * FROM student')
+    data = cur.fetchall()
 
-for line in data:
-    print(line)
+    for line in data:
+        print(line)
 
-# updating record
-curosr.execute('''
-UPDATE student
-SET
-''')
+# Updating record
+def update_UNI():
+    ID = input("Enter your ID: ")
+    nchoice = input("Enter your new University Choice: ")
+    ncourse = input("Enter your new course choice at "+nchoice+": ")
+    
+    cursor.execute('''
+    UPDATE student
+    SET course = ?, choice = ?
+    WHERE id = ?
+    ''',(ncourse,nchoice,ID))
+ 
 
+def update_GRADE():
+    pass
+    
+update_UNI()
 
 
 #close database on script end
