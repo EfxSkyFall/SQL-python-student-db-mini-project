29ec4c7128cbbf7922e38b8686d8768cac5efcdf
EfxSkyFall <19wrobbens@salesian.surrey.sch.uk>
2025-06-10 14:51:59 +0100

    completed project with validation and comments

--- python with sql.py
+++ python with sql.py
@@ -3,7 +3,7 @@ import sqlite3
 db = sqlite3.connect('student_db.db')
 cursor = db.cursor()
 
-# create new table
+#Create new table - Checking if a table called student exists, if not a table is created.
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS student (
     ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -17,17 +17,17 @@ CREATE TABLE IF NOT EXISTS student (
 ''')
 
 db.commit()
-
+#Menu - The general menu, appears after each opearation, used for navigation.
 def menu():
     print("\n Welcome to the superior student managment system!!! \n")
     try:
-        choice = int(input("1. Insert a new record \n2. View the database \n3. Update University and course of choice \n4. Update grades \n5. Delete a record \n"))
+        choice = int(input("\n1. Insert a new record \n2. View the database \n3. Update University and course of choice \n4. Update grades \n5. Delete a record \n6. Exit \n"))
     except:
         print("Wrong type of input: INVALID")
-        choice = int(input("1. Insert a new record \n 2. View the database \n 3. Update University and course of choice \n 4. Update grades \n 5. Delete a record \n"))
+        choice = int(input("\n1. Insert a new record \n 2. View the database \n 3. Update University and course of choice \n 4. Update grades \n 5. Delete a record \n6. Exit \n"))
 
     if choice == 1:
-        insert()
+        insert_record()
     elif choice == 2:
         view_data()
     elif choice == 3:
@@ -35,10 +35,14 @@ def menu():
     elif choice == 4:
         update_GRADE()
     elif choice == 5:
-        delete()
+        delete_record()
+    elif choice == 6:
+        print("Thank you for using my python database managment system")
+        pass
+    
 
-# Inserting record
-def insert():
+#Inserting record - entering a new record into the table.
+def insert_record():
     
     name = input('Enter Name: ')
     course = input('Enter the university course you are applying for: ')
@@ -56,7 +60,7 @@ def insert():
     db.commit()
     menu()
 
-# View database
+#View database - displaying the current records in the database.
 def view_data():
     cursor.execute('SELECT * FROM student')
     data = cursor.fetchall()
@@ -65,9 +69,14 @@ def view_data():
         print(line)
     menu()
 
-# Updating record
+#Updating record - Changing the university and course of the specified ID number.
 def update_UNI():
-    ID = input("Enter your ID: ")
+    try:
+        ID = input("Enter your ID: ")
+    except:
+        print("Wrong type of input: INVALID")
+        ID = input("Enter your ID: ")
+        
     nchoice = input("Enter your new University Choice: ")
     ncourse = input("Enter your new course choice at "+nchoice+": ")
     
@@ -79,9 +88,13 @@ def update_UNI():
     db.commit()
     menu()
  
-
+#Updating record - Changing the grades of a specified ID number.
 def update_GRADE():
-    ID = input("Enter your ID: ")
+    try:
+        ID = input("Enter your ID: ")
+    except:
+        print("Wrong type of input: INVALID")
+        ID = input("Enter your ID: ")
     ngrade1 = input("Renter grade of subject 1: ")
     ngrade2 = input("Renter grade of subject 2: ")
     ngrade3 = input("Renter grade of subject 3: ")
@@ -95,16 +108,21 @@ def update_GRADE():
     menu()
     
     
-    
-def delete():
-    ID = input("Enter the ID of the record you want deleted: ")
+#Deleting a record - removes a record from the table.   
+def delete_record():
+    try:
+        ID = input("Enter the ID of the record you want deleted: ")
+    except:
+        print("Wrong type of input: INVALID")
+        ID = input("Enter the ID of the record you want to be deleted: ")
+        
     cursor.execute("""
     DELETE FROM student
     WHERE id = ?
     """,(ID))
     db.commit()
-    menu
+    menu()
 
 menu()
-#close database on script end
+#Closes the database on script end.
 db.close()
